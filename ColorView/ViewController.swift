ed895791cc8839ca4c6722b20c7abab520e8ff89
Ruslan Bityukov <bitukov_ruslan@mail.ru>
2021-08-21 22:02:35 +0300

    update slider

--- ColorView/ViewController.swift
+++ ColorView/ViewController.swift
@@ -8,12 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    @IBOutlet var redValueLabel: UILabel!
+    @IBOutlet var greenValueLabel: UILabel!
+    @IBOutlet var blueValueLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        colorView.layer.cornerRadius = 10
+        
+        redSlider.value = 0.3
+        greenSlider.value = 0.6
+        blueSlider.value = 0.9
     }
 
-
+    @IBAction func sliderValue(_ sender: UISlider) {
+    
+        redValueLabel.text = String(format: "%.2f", redSlider.value)
+        greenValueLabel.text = String(format: "%.2f", greenSlider.value)
+        blueValueLabel.text = String(format: "%.2f", blueSlider.value)
+        
+        let backgraundColor = UIColor(red: CGFloat(redSlider.value),
+                                      green: CGFloat(greenSlider.value),
+                                      blue: CGFloat(blueSlider.value),
+                                      alpha: 1.0)
+        
+        colorView.backgroundColor = backgraundColor
+    }
 }
 
